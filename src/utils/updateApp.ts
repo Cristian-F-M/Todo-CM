626d2879cd544b5a339ded6f96b36ae0519016d9
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-19 12:19:56 -0500

    Add function to download app from GitHub realeses page, settting progress to global state, and open apk when the download ends

--- src/utils/updateApp.ts
+++ src/utils/updateApp.ts
@@ -1,8 +1,13 @@
+import * as FileSystem from 'expo-file-system'
+import { createDownloadResumable } from 'expo-file-system/legacy'
+import * as Notifications from 'expo-notifications'
+import { ToastAndroid } from 'react-native'
 import { useModal } from '@/state/modal'
 import { useRealese } from '@/state/realese'
 import type { RealeseData } from '@/types/realese'
 import { LOGGER } from '@/utils/logger'
 import pkg from '../../package.json' with { type: 'json' }
+import { getNotificationsPermissions } from './notifications'
 
 type GetLatestAppDataReturn =
 	| {
@@ -46,4 +51,70 @@ export async function checkUpdate() {
 	const t = Number(tag_name.split('.').join(''))
 
 	if (t > v) return openModal('update')
+}
+
+export async function downloadApp() {
+	const { data } = useRealese.getState()
+	const assets = data?.assets ?? []
+	const asset = assets.find((asset) => asset.name.includes('apk'))
+	const { setProgress } = useRealese.getState()
+	let lastPercent = 0
+
+	if (!data || !asset)
+		return ToastAndroid.show('Error al descargar la app', ToastAndroid.SHORT)
+
+	let path: string | FileSystem.File = FileSystem.Paths.join(
+		FileSystem.Paths.cache.uri,
+		`${data.tag_name}.apk`
+	)
+	path = new FileSystem.File(path)
+
+	if (!getIsValidAPK()) {
+		path.delete()
+		path = new FileSystem.File(path)
+	}
+
+	if (path.exists) return installAPK(path.uri)
+
+	setProgress(0)
+	await getNotificationsPermissions()
+
+	const downloadResumable = createDownloadResumable(
+		asset.browser_download_url,
+		path.uri,
+		{},
+		(progres) => {
+			const percent = Math.floor(
+				(progres.totalBytesWritten / progres.totalBytesExpectedToWrite) * 100
+			)
+
+			if (percent !== lastPercent) {
+				lastPercent = percent
+				setProgress(percent)
+			}
+		}
+	)
+
+	try {
+		await downloadResumable.downloadAsync()
+
+		installAPK(path.uri)
+
+		// send notification
+		const id = await Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'Aplicación descargada',
+				body: 'Aplicación descargada'
+			},
+			trigger: null
+		})
+
+		setTimeout(() => {
+			Notifications.dismissNotificationAsync(id)
+		}, 5000)
+	} catch (error) {
+		LOGGER.error(error)
+		ToastAndroid.show('Error al descargar la app', ToastAndroid.SHORT)
+		setProgress(null)
+	}
 }
