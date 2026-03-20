fff503b8228106ad59e190d231fbdd50e150a241
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-20 14:34:24 -0500

    Add incator if need update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
+import { IconDownload } from '@tabler/icons-react-native'
 import { useEffect } from 'react'
-import { FlatList, View } from 'react-native'
+import { FlatList, Pressable, View } from 'react-native'
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated'
 import { FolderItem } from '@/components/folder/FolderItem'
 import { NoFolders } from '@/components/folder/NoFolders'
@@ -7,11 +8,17 @@ import { BackgroundIcon } from '@/components/layout/BackgroundIcon'
 import { FloatingDownload } from '@/components/layout/FloatingDownload'
 import { Header } from '@/components/layout/Header'
 import { Screen } from '@/components/layout/Screen'
+import useReleaseHook from '@/hooks/useRelease'
 import useFolder from '@/state/Folder'
+import { useModal } from '@/state/modal'
+import { useThemeStyles } from '@/utils/theme'
 
 export default function Index() {
 	const { folders } = useFolder()
 	const opacity = useSharedValue(1)
+	const themeStyles = useThemeStyles()
+	const { needUpdate } = useReleaseHook()
+	const { openModal } = useModal()
 
 	const thereIsFolders = folders.length > 0
 
@@ -28,8 +35,24 @@ export default function Index() {
 			<Animated.View style={{ opacity }}>
 				<Header />
 
+				{needUpdate && (
+					<Pressable
+						onPress={() => openModal('update')}
+						className="ml-auto mr-4 p-2 rounded-full justify-center items-center"
+						style={{
+							backgroundColor: themeStyles.surface(),
+							borderWidth: 2,
+							borderColor: themeStyles.border()
+						}}
+					>
+						<View className="mb-px">
+							<IconDownload size={20} color={themeStyles.primary()} />
+						</View>
+					</Pressable>
+				)}
+
 				{thereIsFolders && (
-					<View className="mt-10 items-center justify-center w-11/12 mx-auto mb-6">
+					<View className="mt-4 items-center justify-center w-11/12 mx-auto mb-6">
 						<FlatList
 							className="w-full gap-y-2"
 							data={folders}
