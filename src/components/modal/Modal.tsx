db7b68723fc5bd364dc10f3f9bf00ac30fa41488
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-22 17:12:53 -0500

    Add scrollViewProps only if flatListProps prop is not in props object

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -52,9 +52,11 @@ export function Modal({
 	return (
 		<Portal>
 			<Modalize
-				scrollViewProps={{
-					keyboardShouldPersistTaps: 'handled'
-				}}
+				{...(!props.flatListProps && {
+					scrollViewProps: {
+						keyboardShouldPersistTaps: 'handled'
+					}
+				})}
 				ref={modalRef}
 				modalStyle={modalStyle}
 				adjustToContentHeight
